36390a299ae0473bb517fae39b52ee6d7bbf77d3
Nawaank <mirtindayot@gmail.com>
2026-03-25 12:26:21 +0100

    Ajout des tests avec SchoolClass et étudiants

--- tp.py
+++ tp.py
@@ -11,9 +11,15 @@ class Student:
     def __str__(self):
         return f"{self.name} - Math: {self.math}, Physics: {self.physics}, English: {self.english}"
     
-class Classroom:
+class SchoolClass:
     def __init__(self):
         self.students = []
 
     def add_student(self, student):
         self.students.append(student)
+
+if __name__ == "__main__":
+    school_class = SchoolClass()
+    school_class.add_student(Student('J', 10, 12, 13))
+    school_class.add_student(Student('A', 8, 2, 17))
+    school_class.add_student(Student('V', 9, 14, 14))
